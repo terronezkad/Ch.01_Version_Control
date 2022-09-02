1fdb154bfbff31da95959d0229bb84cfc2da4cb0
terronezkad <terronezkad@urbandaleschools.com>
2022-09-02 09:19:30 -0500

    writes name

--- 1.0_Jedi_Training.py
+++ 1.0_Jedi_Training.py
@@ -1,21 +1,21 @@
 '''
-Sign your name:________________
+Sign your name:Kadin Terronez
 
 
 
-Define Forking: 
+Define Forking: copying a repository to make your own in github
 
-Define Cloning: 
+Define Cloning: sending a repository form your github to pycharm
 
-Define Branching:
+Define Branching: making different copies of code that you can switch back and forth from in pycharm
 
-Define Committing: 
+Define Committing: a way to confirm the changes made in a project or branch
 
-Define Merging: 
+Define Merging: combining to branches to make a finished copy
 
-Define Pushing:
+Define Pushing: sending a finished project to github
 
-Define Pull Request:
+Define Pull Request: requesting the teacher to merger your project into their repository to be tested and graded
 
 
 '''
